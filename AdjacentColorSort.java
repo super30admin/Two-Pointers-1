0d3fda468a1eac2cd0cbc9002bc280e274c82a6c
Arivoli AE <aehari2010@gmail.com>
2019-07-17 21:40:13 -0700

    Commit for Two Pointers 1 - Changes to Sort Colors

--- AdjacentColorSort.java
+++ AdjacentColorSort.java
@@ -1,7 +1,7 @@
 /**
  * Idea: 
- * 1. Count the number of 0s, 1s, and 2s.
- * 2. Iterate over the array again and update array based on counter values
+ * 1. Three pointers each for 0,1 and 2. low, mid are at start and high at end.
+ * 2. If mid element is 0 swap with low, mid is 2 swap with high and update pointer accordingly.
  * 
  * Time Complexity: O(n)
  * Space Complexity: O(1) - no extra space
@@ -10,26 +10,39 @@
 
 class AdjacentColorSort {
 
-    public void sortColors(int[] nums) {
-        int color0=0, color1=0, color2=0;
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == 0) color0++;
-            if(nums[i] == 1) color1++;
-            if(nums[i] == 2) color2++;
+    public void sortColors(int[] nums){
+        int low = 0; // 0
+        int mid = 0; // 1
+        int high = nums.length-1; // 2
+
+        while(mid <= high){
+         if(nums[mid] == 1){
+            mid++;    
+         }   
+         else if(nums[mid] == 0){
+            swap(nums, mid++, low++);
+         }
+         else{
+            swap(nums, mid, high--);
+         }
         }
 
-        for(int i=0;i<color0;i++)
-            nums[i] = 0;
-        for(int i=color0;i<color0+color1;i++)
-            nums[i] = 1;
-        for(int i=color0+color1;i<color0+color1+color2;i++)
-            nums[i] = 2;
+        for(int i=0;i<nums.length;i++){
+            System.out.println(nums[i]);
+        }
     }
     public static void main(String[] args){
         System.out.println("Adjacent Color Sort");
         AdjacentColorSort obj = new AdjacentColorSort();
         int[] nums = {2,0,2,1,1,0};
         obj.sortColors(nums);
+
+        //System.out.println()
     }
 }
