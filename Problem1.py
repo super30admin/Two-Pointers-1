09dc285012307c574bd3e42725c3953a8734dd44
Mudita Bysani <muditabysani@muditas-mbp.lan>
2019-08-14 19:01:41 -0700

    Added optimized solutions

--- Problem1.py
+++ Problem1.py
@@ -1,5 +1,5 @@
 class Problem1:
-	def SortColors(self, arr):
+	def SortColors1(self, arr):
 		# Time Complexity : O(n) where n is the seize of the array
 		# Space Complexity : constant space. Because we have to store three colors and count for each of them. The size of hashmap is always three or less than three.
 		# Passed on Leetcode
@@ -17,8 +17,26 @@ class Problem1:
 				arr[j] = i
 				j += 1
 
+	def SortColors2(self, arr):
+		low = 0
+		mid = 0
+		high = len(arr) - 1
+		while mid < high:
+			if arr[mid] == 1:
+				mid += 1
+			elif arr[mid] == 0:
+				arr[low], arr[mid] = arr[mid], arr[low]
+				low += 1
+				mid += 1
+			else:
+				arr[high], arr[mid] = arr[mid], arr[high]
+				high -= 1
+
 if __name__ == '__main__':
 	sc = Problem1()
 	arr = [2,0,2,1,0,1]
-	sc.SortColors(arr)
+	sc.SortColors1(arr)
+	print(arr)
+	arr = [2,0,2,1,0,1]
+	sc.SortColors2(arr)
 	print(arr)
--- Problem2.py
+++ Problem2.py
@@ -1,5 +1,5 @@
 class Problem2:
-	def ThreeSum(self, arr):
+	def ThreeSum1(self, arr):
 		# Time complexity : O(n^3) where n is the number of elements in the array
 		# Space complexity : result array. No extra space being used
 		# Finding every 3 elements combination from the array and adding it to the resultant array only if the sum is equal to zero
@@ -16,6 +16,31 @@ class Problem2:
 							res.append(temp)
 		return res
 
+	def ThreeSum2(self, arr):
+		res = []
+		arr.sort()
+		for i in range(len(arr)):
+			low = i + 1
+			high = len(arr) - 1
+			if i > 0. and arr[i] == arr[i - 1]:
+				continue
+			while low < high:
+				temp_sum = arr[i] + arr[low] + arr[high]
+				if temp_sum == 0:
+					res.append([arr[i], arr[low], arr[high]])
+					low += 1
+					high -= 1
+					while low < high and arr[low] == arr[low - 1]:
+						low += 1
+					while low < high and arr[high] == arr[high + 1]:
+						high -= 1
+				elif temp_sum > 0:
+					high -= 1
+				else:
+					low += 1
+		return res
+
 if __name__ == '__main__':
 	ts = Problem2()
-	print(ts.ThreeSum([-1, 0, 1, 2, -1, -4]))
+	print(ts.ThreeSum1([-1, 0, 1, 2, -1, -4]))
+	print(ts.ThreeSum2([-1, 0, 1, 2, -1, -4]))
