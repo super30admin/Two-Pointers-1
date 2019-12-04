ecc15539150c4ec95cb33ac81790503c1e1eff2d
Akhil Raveendran <itsrakhil163@gmail.com>
2019-12-04 01:52:27 -0800

    Create maxArea.java

---
+++ maxArea.java
@@ -0,0 +1,15 @@
+Time Complexity-O(n)
+Space Complexity-O(1)
+public class Solution {
+    public int maxArea(int[] height) {
+        int max = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            max = Math.max(max, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return max;
+    }
+}
