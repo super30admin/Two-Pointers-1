cc640d80f3096c5e9c8d34dcc69f8103064f51cf
Siyami Avci <siyamiavci@gmail.com>
2020-05-06 15:34:34 -0700

    add solution 3

---
+++ problem1.js
@@ -0,0 +1,9 @@
+// will finish after class
+
+// # Two-Pointers-1
+
+// ## Problem1 (https://leetcode.com/problems/sort-colors/)
+
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
---
+++ problem2.js
@@ -0,0 +1,4 @@
+// will finish after class
+
+// ## Problem2 
+// (https://leetcode.com/problems/3sum/)
