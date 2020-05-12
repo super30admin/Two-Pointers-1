8da660333b76b17d314e4e692510d8ee5391305d
Siyami Avci <siyamiavci@gmail.com>
2020-05-12 14:57:06 -0700

    add problem1

--- problem1.js
+++ problem1.js
@@ -1,9 +1,30 @@
-// will finish after class
+// ## Problem1 
+// (https://leetcode.com/problems/sort-colors/)
 
-// # Two-Pointers-1
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
 
-// ## Problem1 (https://leetcode.com/problems/sort-colors/)
-
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+const swap = function(nums, i, j) {
+    let temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+const sortColors = function(nums) {
+    if(!nums || nums.length === 0) return nums;
+    let left = 0, mid = 0, right = nums.length - 1;
+    let temp = 0;
+    while(mid <= right) {
+       if(nums[mid] === 1) mid++;
+        else if (nums[mid] === 2) {
+            swap(nums, mid, right);
+            right--;
+        }
+        else {
+            swap(nums, left, mid);
+            left++;
+            mid++;
+        }
+    }
+    return nums;           
+};
