b6dfa986076762910dc721e17e03aefad28efbcd
Meghna Singhal <meghnasinghal2@gmail.com>
2020-06-10 17:41:46 -0500

    Completed Two-Pointers-1- PLease Review

--- Sample.java
+++ Sample.java
@@ -66,3 +66,73 @@ class Solution {
         return max;
     }
 }
+
+//3 sum
+// Time Complexity : O(nlogn + n^2) = O(n^2)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : None
+
+
+// Your code here along with comments explaining your approach
+
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+     List<List<Integer>> list = new ArrayList<>();
+        
+        if(nums==null || nums.length <3) // if length<3 then no triplets
+            return new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums); // first sort the array
+        
+      for(int i=0;i<n-2;i++) // to keep track of outer index
+      {
+         int low = i+1;
+         int high = n-1;
+         
+          //duplicates check for outer index
+          if(i> 0 && nums[i] == nums[i-1])
+              continue; // if both value same, no need to check
+          
+          if(nums[i] > 0)
+              break; // wont find sum 0 in a sorted array after 0 as values more than 0
+    
+          while(low<high) // can't consider value again so no equal check
+          {
+              int sum = nums[i] + nums[low] + nums[high]; // finding sum
+              if(sum == 0) // checking sum with target
+              {
+                List<Integer> li= Arrays.asList(nums[i],nums[low],nums[high]); // making a list
+                  Collections.sort(li); // sorting
+                    list.add(li); // adding to final list
+                    // here no need to check if list already contains the triplet as duplicacy has been checked at indices
+                  
+                  low++; // more triplets could be present
+                  high--;
+                  
+                  //duplicates check for inner index 
+                  while(low<high && nums[low] == nums[low-1])
+                  {
+                      low++;
+                  }
+                  while( low<high && nums[high] == nums[high+1])
+                  {
+                      high--;
+                  }
+              }
+              else if(sum<0) // if sum > 0 
+              {
+                  low++; // need to increase sum so increment low
+              }
+              else
+              {
+                  high--; // need to decrease sum so decrement high
+              }
+          }
+      }
+        return list;
+        
+    }
+}
