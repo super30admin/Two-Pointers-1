3285f35f21c1632f017c91c1f139d36b973c63f7
chirag <cr.tagadiya@gmail.com>
2020-06-11 15:23:46 -0400

    3sum better approach done

--- 3SUM.java
+++ 3SUM.java
@@ -1,7 +1,12 @@
 /*
-    Time Complexity : O(N^3)
-    Space Complexity :O(N)
-    is worked on leetcode  : NO Time limit Exceeded
+    Brute Force Complexity :
+        Time Complexity : O(N^3)
+        Space Complexity :O(N)
+        is worked on leetcode  : NO Time limit Exceeded
+    Efficient Approahc:
+        Time Complexity : O(N^2)
+        space complexity : O(1)
+        is worked on leetcode : YES
 
 */
 
@@ -10,6 +15,7 @@
 */
 
 public class 3SUM{
+    
     public List<List<Integer>> threeSum(int[] nums) {
         if  (nums == null || nums.length == 0){
             return new ArrayList<List<Integer>>();
@@ -34,5 +40,52 @@ public class 3SUM{
         return  new ArrayList<List<Integer>>(al);
     }
 
+
+    /*
+        Two pointer Approach
+
+    */
+    public List<List<Integer>> threeSum(int[] nums) {
+        if  (nums == null || nums.length == 0){
+            return new ArrayList<List<Integer>>();
+        }
+       
+        HashSet<List<Integer>> set = new HashSet<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<n-2;i++){
+            int low = i+1;
+            int high = n-1;
+        
+            if(i >0 && nums[i] == nums[i-1]) continue;
+            
+            if(nums[i] > 0) break;
+            
+            while(low <high){
+                int sum = nums[i] + nums[low] +  nums[high];
+                if(sum == 0){
+                    ArrayList<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[i]);
+                    list.add(nums[low]);
+                    list.add(nums[high]);
+                    Collections.sort(list);
+                    set.add(list);
+                    low++;
+                    high--;
+                    while(low < high && nums[low-1] == nums[low]) low++;
+                    while(high > 0 && nums[high+1] == nums[high]) high--;
+                }else if(sum < 0){
+                    low++;
+                    while(low < high && nums[low-1] == nums[low]) low++;
+                }else{
+                    high--;
+                while(high > 0 && nums[high+1] == nums[high]) high--;
+                }
+            }
+            
+        }
+        return new ArrayList<>(set);
+    }
 }
+
 
