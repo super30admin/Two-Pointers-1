c907f3d21df9ec7c12a50261981e7ff88fe96494
Shashank Baghel <41822011+Shashank001122@users.noreply.github.com>
2020-06-14 22:44:57 -0700

    Create Changecolor.py

---
+++ Changecolor.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        low=0
+        mid=0
+        high=len(nums)-1
+        while(mid<high):
+            if nums[mid]==1:
+                mid+=1
+            if nums[mid]==2:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
+            
+            if nums[mid]==0:
+                nums[low],nums[mid]=nums[mid],nums[low]
+                low+=1
+                mid+=1
+ time complexity is O(n)
+ space is O(1)
