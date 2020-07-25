24ba2df2357ec6807bda417a0f4422d1bb3e369d
Arpit Sharma <arpit317@gmail.com>
2020-07-24 18:56:41 -0700

    Update ContainerWithMostWater.java

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -1,3 +1,10 @@
+//TC: O(n)
+//SC: O(1)
+//Approach:
+// As we need to get the max area, and smaller height gets to decide the area for that particular combination, while iterating, we move that pointer which points
+// to the smaller height from low and high. This way, we make sure that by moving smaller height pointer, I may get towards bigger heights and hence bigger area
+// or if there are no bigger heights, then I already have the bigger area.
+
 class Solution {
     public int maxArea(int[] height) {
         int res = 0;
