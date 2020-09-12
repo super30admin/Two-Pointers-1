e9032ed7dc263bc0c7d213a45681a058a1b52352
asiddiqua004 <48198420+asiddiqua004@users.noreply.github.com>
2020-09-12 14:05:15 -0700

    Update sort_colors.cpp

--- sort_colors.cpp
+++ sort_colors.cpp
@@ -56,3 +56,53 @@ public:
         return sort_using_two_pointers(nums);
     }
 };
+
+
+// Time Complexity : O(n * (k-1))
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+
+// Your code here along with comments explaining your approach
+
+//1.Edge case
+//A. return when size is 0 or 1
+//2. Sort
+//A. For each (K-1) color we have to go throiugh the array 
+//B check if first pointer is at desired color, if yes then increment first pointer
+//C. check if second pointer is at desired color then swap with two pointers, increment first pointer to point to non-color element
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        //edge
+        if(nums.size()==0 || nums.size()==1)
+            return;
+        
+        //logic
+        int color = 0;
+        int first_ptr =0;
+        int second_ptr=0;
+        int size = nums.size();
+        
+        while (color<2){
+            //2.B
+            while(first_ptr<size && nums[first_ptr] == color){
+                first_ptr++;
+                //cout<<"here "<<first_ptr<<endl;
+            }
+            //2.C
+            for(second_ptr =first_ptr+1; second_ptr<size; second_ptr++ ){
+                if(nums[second_ptr] == color){
+                    swap(nums[second_ptr], nums[first_ptr]);
+                    first_ptr++;
+                }
+                //cout<<"first "<< first_ptr<<"second "<<second_ptr<<endl;
+            }
+            color++;
+        }
+        
+        return;
+    }
+};
