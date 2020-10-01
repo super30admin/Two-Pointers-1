c61f24c76e259a98f66ce433ff6e874d9752addd
koshyanand <koshyanand@gmail.com>
2020-09-30 20:49:17 -0700

    Completed 2

--- Problem2.py
+++ Problem2.py
@@ -1,22 +1,40 @@
 # Time Complexity: O(N^2)
 # Space Complexity: O(N)
-# Did not pass leetcode. Need to optimize further to handle duplicates
+# Pass leetcode.
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
-        m = set(nums)
+        nums.sort()
         out = []
-        for i in range(len(nums)):
+        for i in range(len(nums) - 2):
             
-            for j in range(i + 1, len(nums)):
-                
-                curr = nums[i] + nums[j]
-                
-                if curr != 0:
-                    curr = -curr
+            if i > 0 and nums[i - 1] == nums[i]:
+                    continue
+            left = i + 1
+            right = len(nums) - 1
+            
+            while left < right:
+
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    out.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
                     
-                if curr in m:
-                    out.append([nums[i], nums[j], curr])
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1  
+                    
+                # elif total < 0 and nums[left] > 0:
+                #     break
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+                
+        
         return out
+                
                 
