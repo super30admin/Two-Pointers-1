0ac33e9eecfe024f1e49753b853042a74a7e57ec
atolat <arpantolat30@gmail.com>
2020-10-27 17:43:30 -0700

    Second Pass

--- 3sum.py
+++ 3sum.py
@@ -1,3 +1,12 @@
+# Time Complexity: O(n log n) + O(n^2) -> O(n^2)
+# Space Complexity: O(1), no auxillary space used
+# Did it run on Leetcode: Yes
+# Approach: 
+# Using an approach similar to the two sum problem to find pairs that add up to a target
+# The function findPairs uses two pointers and iterates over the array to find two number + a fixed 'first' number that add up to the target
+# Call this function for every number in the array
+# More comments in-line...
+
 class Solution(object):
     def threeSum(self, nums):
         """
@@ -5,35 +14,45 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         def findPairs(first, target):
+            """
+            first: A fixed number
+            target: The number the triplets must add up to
+            This function finds two numbers + 'first' that add up to the target
+            """
             start = first + 1
             end = len(nums) - 1
             
             while start < end:
                 triplet_sum = nums[first] + nums[start] + nums[end]
                 if triplet_sum == target:
-                    
                     triplets.append([nums[first], nums[start], nums[end]])
                     start += 1
                     end -= 1
+                    # To skip over duplicates, increment start, decremnent end
                     while start < end and nums[start] == nums[start-1]:
                         start += 1
                     while start < end and nums[end] == nums[end+1]:
                         end -= 1
                 else:
-                    if triplet_sum < target:
+                    if triplet_sum < target: # Increment the start if sum is lesser than target
                         start += 1
-                    else:
+                    else: # decrement end to bring sum down
                         end -= 1
         
+        # Edge Cases
         if not nums or len(nums) < 3:
             return []
         if len(nums) == 3:
             return [[nums[0], nums[1], nums[2]]] if sum(nums) == 0 else []
         
+        # Array to store results
         triplets = []
+        
+        # Sort input array
         nums.sort()
         
         for i in range(len(nums)):
+            # Skip over duplicates
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             findPairs(i, 0)
--- sort-colors.py
+++ sort-colors.py
@@ -1,25 +1,28 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Did this run on Leetcode: Yes
+# Approach: Similar to 3 way partitioning routine for quicksort
+# Use two pointers to keep track of the low and high boundaries. Set '1' to be the pivot element
+# Iterate over the array, if the element is < pivot, swap the number with low pointer element - expand the low boundry outwards
+# If the element is > pivot, swap it with the high pointer element, shrink the high boundry inwards
+# Continue till the array is partitioned into low::pivot::high -- i crosses over high
+
 class Solution(object):
     def sortColors(self, nums):
         """
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        # 0 - red
-        # 1 - white
-        # 2 - blue
-        
-        low, i = 0, 0
+        i, low = 0, 0
         high = len(nums) - 1
-        
+        pivot = 1
         while i <= high:
-            if nums[i] == 0:
-                nums[i], nums[low] = nums[low], nums[i]
-                low += 1
+            if nums[i] < pivot:
+                nums[low], nums[i] = nums[i], nums[low]
                 i += 1
-            elif nums[i] == 2:
+                low += 1
+            elif nums[i] > pivot:
                 nums[i], nums[high] = nums[high], nums[i]
                 high -= 1
-            else:
+            else: # nums[i] == pivot
                 i += 1
-                
-        return nums
