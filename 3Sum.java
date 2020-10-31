95709498665e4c40580c6d8258d2437af60a598c
harsh <harshsaini697@gmail.com>
2020-10-30 23:01:50 -0500

    T

--- 3Sum.java
+++ 3Sum.java
@@ -1,4 +1,4 @@
-// Time Complexity :  O(N^3) --3 O(N) * O(N - 1) * O(N - 2) ~ N^3 
+// Time Complexity :  O(N^2)
 // Space Complexity : O(N) -- set O(N)
 // Did this code successfully run on Leetcode :Yes
 // Any problem you faced while coding this :No
@@ -12,9 +12,11 @@ class Solution {
         List<List<Integer>> set = new ArrayList<>();
         Arrays.sort(nums);
         for(int i = 0; i < nums.length - 2; i++){
+            if(nums[i]>0) break;
             if(i>0 && nums[i] == nums[i-1]) continue;
             int low = i + 1;
             int high = nums.length - 1;
+            
             while(low < high){
                 int sum = nums[i] + nums[low] + nums[high];
                 if(sum == 0){
