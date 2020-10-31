64259b95661885ddef3369da947e4990a2c4436a
harsh <harshsaini697@gmail.com>
2020-10-30 22:52:25 -0500

    adding optimization in 3Sum

--- 3Sum.java
+++ 3Sum.java
@@ -19,6 +19,8 @@ class Solution {
                 if(sum == 0){
                     set.add(Arrays.asList(nums[i], nums[low], nums[high]));
                     low++;high--;
+                    while(low < high && nums[low] == nums[low -1]) low++;
+                    while(low < high && nums[high] == nums[high+ 1]) high--;
                 }
                 else if(sum < 0){
                     low++;
