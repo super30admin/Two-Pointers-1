2fe5d519bb85cc85232b405c32e612a473d259f0
Ashish Mishra <ashishk861@gmail.com>
2021-01-06 16:38:31 -0500

    Update Problem_3.java

--- Problem_3.java
+++ Problem_3.java
@@ -1,7 +1,22 @@
-// Time Complexity :
-// Space Complexity :
+// Time Complexity :O(n^2)
+// Space Complexity :O(1)
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public int maxArea(int[] height) {
+        int maxValue=0;
+        int min=0;
+        for(int i=0;i<height.length;i++){
+            for(int j=i+1;j<height.length;j++){
+                min=Math.min(height[i],height[j]);
+                if((min*(j-i))>maxValue){
+                    maxValue=min*(j-i);
+                }
+            }
+        }
+        return maxValue;
+    }
+}
