da65cf47a4f993b24e40887c5f17ee44b01fda1d
Punyabhuarj <76171476+Punyabhuarj@users.noreply.github.com>
2021-01-19 02:47:11 +0530

    Add files via upload

---
+++ solution3.java
@@ -0,0 +1,29 @@
+// class solution time and space of constant
+public class solution3 {
+	 public void sortColors(int[] nums) {
+	        //Base case
+	        if(nums == null || nums.length ==0)
+	            return;
+	        int left = 0;
+	        int curr = 0;
+	        int right = nums.length - 1;
+
+	        while(curr<= right){
+	            if(nums[curr]==1)
+	                curr++;
+	            else if(nums[curr]==0)
+	                //see optimization
+	                swap(nums,left++,curr++) ;
+	            else
+	                swap(nums,curr,right--);
+
+	        }
+	    }
+	    private void swap(int[] nums, int i, int j){
+	        int temp = nums[i];
+	        nums[i] = nums[j];
+	        nums[j] = temp;
+	    }
+	} 
+
+
