4b3031e467be1c23878e8df501c8a21a43338470
Kapish Garg <kapish5467@gmail.com>
2021-02-03 12:17:36 -0800

    Completed

--- Problem2.java
+++ Problem2.java
@@ -1,38 +1,68 @@
 // Time Complexity :O(n^2)
-// Space Complexity :O(n)
-// Did this code successfully run on Leetcode :No
-// Any problem you faced while coding this :Not able to form unique pairs of list.
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :No
 
 
 // Your code here along with comments explaining your approach
-    //Each element's negative value is the target sum for the rest of the array.
-    //We approach with two sum and find the list, but right now I have duplicate list in my final output.
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        if(nums==null || nums.length==0 )
+    //Sort the array
+    //Find the sum of three pointers,if it is greater than zero decrease last pointer else increase first pointer.
+    class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+        if(nums==null || nums.length<3)
             return new ArrayList<>(0);
-        
-        List<List<Integer>> out= new ArrayList<>();
-        for(int j=0;j<nums.length;j++)
-        {
-            int target=-nums[j];
-            HashMap<Integer,Integer> map= new HashMap<>();
-            for(int i=j+1;i<nums.length;i++)
-                {                  
-                    if(map.containsKey(nums[i]))
+            
+            Arrays.sort(nums);
+            
+    
+            List<List<Integer>> out = new ArrayList<>();
+            for(int i=0;i<nums.length-2;i++)
+            {
+                if(i!=0 && nums[i-1]==nums[i])
+                    continue;
+            
+                    int j=i+1;
+                    int k=nums.length-1;
+                while(j<k){
+                    int sum=nums[i]+nums[j]+nums[k];
+                
+                 if(sum==0)
                     {
-                        List<Integer> list= new ArrayList<>();
-                        list.add(nums[map.get(nums[i])]);
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        out.add(list);
+                        out.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                        j++;
+                        k--;
+                    
+                    while(j<k &&  nums[j-1]==nums[j])
+                    {
+                        j++;
                     }
-                    else
-                        map.put(target-nums[i],i);
+                    while(j<k && nums[k+1]==nums[k])
+                    {
+                        k--;
+                    }
+                 }
+                else if(sum<0)
+                {
+                    j++;
+                    while(j<k  && nums[j-1]==nums[j])
+                    {
+                        j++;
+                    }
                 }
-
-         }
-         return out;
+                else
+                {
+                    k--;
+                     while(j<k && nums[k+1]==nums[k])
+                    {
+                        k--;
+                }
+                
+                }
+            }
+            }
+            return out;
         }
-       
-    }
+            
+           
+        }
+    
