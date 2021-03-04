f7eaebc1b84424468b0384f8b383c3cac4cc1e8f
Tirthaj Rikame <tirthaj.rikame@gmail.com>
2021-03-04 17:43:33 +0530

    Completed 3Sums Problem

--- 3Sums.py
+++ 3Sums.py
@@ -1,7 +1,7 @@
 # Brute Force Approach
 # Time Complexity: O(n^3)
 # Space Complexity: O(1)
-class Solution:
+'''class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
         li = []
@@ -18,4 +18,36 @@ class Solution:
                         if li not in res:
                             res.append(li)
 
+        return res
+'''
+# Optimised Approach
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        if nums == None or len(nums) == 0:
+            return res
+        nums.sort()
+        for i in range(len(nums) - 2):
+            if nums[i] == nums[i - 1] and i > 0:
+                continue
+            low = i + 1
+            high = len(nums) - 1
+            while (low < high):
+                Sum = nums[i] + nums[low] + nums[high]
+                if Sum == 0:
+                    li = [nums[i], nums[low], nums[high]]
+                    low += 1
+                    high -= 1
+                    res.append(li)
+                    while nums[low] == nums[low - 1] and low < high:
+                        low += 1
+                    while nums[high] == nums[high + 1] and low < high:
+                        high -= 1
+
+                elif Sum < 0:
+                    low += 1
+                else:
+                    high -= 1
         return res
