8601a58f72b5c46b034ba3c0c0300586bb319c2d
sidd315 <55456130+sidd315@users.noreply.github.com>
2021-05-05 18:22:00 -0500

    Create sortColors.py

---
+++ sortColors.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        low, mid, high = 0,0,len(nums)-1
+        
+        while mid<=high:
+            if nums[mid] == 0:
+                nums[mid],nums[low] = nums[low],nums[mid]
+                low+=1
+                mid+=1
+            
+            elif nums[mid] == 1:
+                mid+=1
+            
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high-=1
+            
+        
+#  Three pointer solution, one thing to note is mid is moved when 0 is encountered is that if we dont put this condition mid will be left behind low and break the system
+
+#  time complexity is O(n)
+        
+        
