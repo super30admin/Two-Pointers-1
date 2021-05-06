0737c147b821bcab5ba0abb7041d6cbd0305e3f0
sureshmurali710 <54928199+sureshmurali710@users.noreply.github.com>
2021-05-05 23:02:49 -0400

    Create Container_with_most_array.java

---
+++ Container_with_most_array.java
@@ -0,0 +1,24 @@
+//Time Complexity: o(n)
+//Space Complexity: o(1)
+
+class Solution {
+    public int maxArea(int[] height) {
+        if(height == null || height.length == 0) return 0;
+        int max = 0;
+        int left = 0; int right = height.length-1;
+        
+        while(left < right){
+            max = Math.max(max, (right - left)*Math.min(height[left], height[right]));
+            
+            if(height[left] <= height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }   
+            
+        }
+        return max;
+        
+    }
+}
