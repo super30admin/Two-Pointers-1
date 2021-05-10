08930bf1a8f5a09fe9d97ce8b6d0006b2c1922d6
Beryl Mario Shairu Joseph Antony Bose <44846387+Beryl2208@users.noreply.github.com>
2021-05-09 21:40:47 -0400

    Completed Pointers-1

---
+++ P1.java
@@ -0,0 +1,36 @@
+//time-o(n)
+//space-o(1)
+//passed in leetcode- yes
+
+class Solution {
+    public void sortColors(int[] nums) {
+        if(nums==null || nums.length==0) return;
+        int low=0, mid=0;
+        int high=nums.length-1;
+        
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(mid,low,nums);
+                mid++; low++;
+            }
+            else if(nums[mid]==2)
+            {
+                swap(mid,high,nums);
+                high--;
+            }
+            else
+            {
+                mid++;
+            }            
+        }
+    }
+    
+    public void swap(int i, int j, int[] nums)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j]; 
+        nums[j] = temp;
+    }
+}
