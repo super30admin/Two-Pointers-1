a57231673a9d926f34eb7598f0e7a16600d1f89b
shubhamgupta18 <shubhamgupta202017@gmail.com>
2021-05-11 01:10:37 -0500

    Update sortColors.java

--- sortColors.java
+++ sortColors.java
@@ -1,5 +1,54 @@
-package Default;
-
-public class sortColors {
-
+//Time complexity - O(n)
+// Space complexity - O(1)
+//Did this code successfully run on Leetcode : Yes
+//Any problem you faced while coding this : No
+//idea is to use 3 pointers. Left collects all the 0's. Mid iterates and collects all 1's. Right collects all 2's
+class Solution {
+    
+    static void swap(int[] arr, int num1, int num2){
+        
+        int temp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
+    }
+    public void sortColors(int[] nums) {
+        
+        if(nums.length == 0 || nums == null){return;}
+        
+        int left = 0;
+        int mid = 0;
+        int right = nums.length - 1;
+        
+        
+       while(mid <= right){
+            
+            if(nums[mid] == 0){
+                
+                swap(nums,mid,left);
+                
+                left ++;
+                mid ++;
+              
+                
+            }
+            
+            else if(nums[mid] == 2){
+                
+                  swap(nums,mid,right);
+                
+               
+                right --;
+               
+            }
+            
+            else if(nums[mid] == 1){
+         //         swap(nums,nums[mid],nums[right]);
+                
+                mid ++;
+               
+                
+            }
+        }
+        
+    }
 }
