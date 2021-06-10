6f693237d3c7765df84d621ea4c41f92a18d8f51
pratikshakapse5 <40233432+pratikshakapse5@users.noreply.github.com>
2021-06-10 16:44:08 -0700

    Update mostWater.cpp

--- mostWater.cpp
+++ mostWater.cpp
@@ -5,3 +5,33 @@ calculate the current area - that would be the height of smallest container and 
 move the pointer with lesser height
 parallely keep track of the maximum area along 
 */
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+     
+        int maxx=0;
+        int i=0; 
+        int area;
+        int min1;
+        int j=height.size()-1;
+        while(i<j)
+        {
+           if(height[i]<height[j])
+           {
+               min1=height[i];
+                 area=min1*(j-i);
+               i++;
+           }
+            else
+            {
+                min1=height[j];
+                  area=min1*(j-i);
+                j--;
+            }
+          
+            
+           maxx=max(area,maxx);
+        }
+    return maxx;    
+    }
+};
