5497bbc090c3c80ed81532fdc815b0fe826facdb
sahil1291sharma <sahil1291sharma@users.noreply.github.com>
2021-06-10 01:49:14 -0400

    max water update

--- problem3.py
+++ problem3.py
@@ -1,3 +1,5 @@
+#Brute force solution
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         vol = 0
@@ -9,3 +11,27 @@ class Solution:
     
 #time copmlexity is O(n^2)
 #space complexity is O(1)
+
+
+#Two pointer solution
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxArea = 0
+        low = 0
+        high = len(height)-1
+        
+        while low < high:
+            area = (min(height[low], height[high])*(high-low))
+            maxArea = max(maxArea, area)
+            if height[low] < height[high]:
+                low += 1
+            else:
+                high -= 1
+        
+        return (maxArea)
+        
+    
+#time copmlexity is O(n) where n is the number of elements in the list
+#space complexity is O(1)
+# 2 pointer approach is used where the pointer with lesser value is moved towards a higher value index
