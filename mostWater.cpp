404741d87cf3679dd9e72dfeb7d25d96dfa59dac
pratikshakapse5 <40233432+pratikshakapse5@users.noreply.github.com>
2021-06-10 16:43:59 -0700

    Update mostWater.cpp

--- mostWater.cpp
+++ mostWater.cpp
@@ -1,1 +1,7 @@
 
+/*
+idea here is to make use of two pointers one starting at the 0th index and another at the end
+calculate the current area - that would be the height of smallest container and the distance between two pointers
+move the pointer with lesser height
+parallely keep track of the maximum area along 
+*/
