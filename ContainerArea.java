d02dcc13126db2340770c86b67c508fc08a7fec7
Dhanashree Kamath <dhanashreekamath92@gmail.com>
2021-06-10 18:59:36 -0700

    container with most water two pointer solution

--- ContainerArea.java
+++ ContainerArea.java
@@ -1,12 +1,10 @@
-// Time Complexity : O(n^2)
+// Time Complexity : O(n)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this : no
 
-
 class Solution {
     public int maxArea(int[] height) {
-      //Bruteforce solution
       //edge case
       if(height == null || height.length == 0) {
         return 0;
@@ -14,13 +12,15 @@ class Solution {
       
       int max = 0;
       int n = height.length;
-      for(int i = 0; i< n-1; i++) {
-        for(int j = i+1; j<n; j++) {
-          //take two heights and get the area and compare with prev max
-          max = Math.max(max, (j-i)*Math.min(height[i], height[j]));
+      int low = 0; int high = n-1;
+      while(low <= high) {
+        max = Math.max(max, (low-high)*Math.min(height[low],height[high]));
+        if(height[low] < height [high]) {
+          low ++;
+        } else {
+          high --;
         }
       }
-        
       return max;
     }
 }
