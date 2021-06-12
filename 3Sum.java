e69839906c592575f7180bc72aa8e67a7234e701
Dhanashree Kamath <dhanashreekamath92@gmail.com>
2021-06-12 16:09:24 -0700

    3sum using 2 pointers

--- 3Sum.java
+++ 3Sum.java
@@ -1,28 +1,40 @@
-// Time Complexity : O(n^3)
-// Space Complexity : O(n)
+// Time Complexity : O(n^2)
+// Space Complexity : O(1)
 // Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this : no
 
 class Solution {
-    // using nested iterations
     public List<List<Integer>> threeSum(int[] nums) {
+      //result arr
+      List<List<Integer>> result = new ArrayList<>();
       //Edge case
-      if(nums == null || nums.length == 0) return new ArrayList<>();
+      if(nums == null || nums.length < 3) return new ArrayList<>();
       
       int n = nums.length;
-      HashSet<List<Integer>> set = new HashSet<>();
-      for(int i = 0;i < n-2;i++) {
-        for(int j = i+1;j < n-1; j++) {
-          for(int k = j+1 ; k< n; k++) {
-            //check each element with every other 2 number that makes the sum 0
-            if(nums[i] + nums[j] + nums[k] == 0) {
-              List<Integer> li = Arrays.asList(nums[i],nums[j],nums[k]);
-              Collections.sort(li);
-              set.add(li);
-            }
+      Arrays.sort(nums);
+      for(int i = 0; i< n; i++) {
+        //outside duplicate
+        if(i!=0 && nums[i] == nums[i-1]) continue;
+        int left = i+1; int right = n-1;
+        while(left < right) {
+          //check the sum 
+          int sum = nums[i] + nums[left] + nums[right];
+          if(sum == 0) {
+            List<Integer> li = Arrays.asList(nums[i],nums[left],nums[right]);
+            result.add(li);
+            left ++;
+            right --;
+            //inside duplicate
+            while(left < right && nums[left] == nums[left-1]) left++;
+            while(left < right && nums[right] == nums[right +1] )right--;
+          } else if(sum < 0) {
+            left ++;
+          } else {
+            right --;
+          }
           }
         }
+            return result;
       }
-        return new ArrayList<>(set);
-    }
+  
 }
