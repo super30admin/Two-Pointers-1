f7d84f9346529953ca5968a388697ac395b5ac71
pratikshakapse5 <40233432+pratikshakapse5@users.noreply.github.com>
2021-06-12 23:45:31 -0700

    Update sum3.cpp

--- sum3.cpp
+++ sum3.cpp
@@ -1,3 +1,8 @@
 
-/* leetcode 3 sum problem */
+/* leetcode 3 sum problem 
+Idea here is to use Two pointers approach - with sorting
+I sorted array before which takes O(nlogn) and then putting pivot at 0 and low running from pivot+1
+High pointer runs backwards. for every pivot check the sum of the triplet equals 0, less than 0 or greater than 0
+if sum ==0 add  the triplet to result and increase low, decrease high
+*/
 
