de954d2b31d2492a8cfc13ba4009dac7e81540af
pratikshakapse5 <40233432+pratikshakapse5@users.noreply.github.com>
2021-06-12 23:46:37 -0700

    Update sum3.cpp

--- sum3.cpp
+++ sum3.cpp
@@ -6,3 +6,45 @@ High pointer runs backwards. for every pivot check the sum of the triplet equals
 if sum ==0 add  the triplet to result and increase low, decrease high - here checked duplicacy with low and high
 if sum >0 decrease high, if sum<0 increase low */
 
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        
+       int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++)
+        {
+             
+            if(i!=0 and nums[i]==nums[i-1]) continue;
+            int low=i+1;
+             int high=n-1;
+            while(low<high)
+            {
+                int sum=nums[i]+nums[low]+nums[high];
+               
+                if(sum==0)
+                {
+                     vector<int> triplet={nums[i], nums[low], nums[high]};
+                    res.push_back(triplet);
+                    low++;
+                    high--;
+                    while(low<high and nums[low]==nums[low-1]) low++;
+                    while(low<high and nums[high]==nums[high+1]) high--;
+                }
+                else if(sum>0)
+                {
+                    high--;
+                }
+                else
+                {
+                    low++;
+                }
+                 
+            }
+            
+           
+        }
+        return res;
+    }
+};
