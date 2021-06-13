d00eb8d8b35bf6d6601a46e08229d3eef3bc60b0
pratikshakapse5 <40233432+pratikshakapse5@users.noreply.github.com>
2021-06-12 23:46:23 -0700

    Update sum3.cpp

--- sum3.cpp
+++ sum3.cpp
@@ -3,6 +3,6 @@
 Idea here is to use Two pointers approach - with sorting
 I sorted array before which takes O(nlogn) and then putting pivot at 0 and low running from pivot+1
 High pointer runs backwards. for every pivot check the sum of the triplet equals 0, less than 0 or greater than 0
-if sum ==0 add  the triplet to result and increase low, decrease high
-*/
+if sum ==0 add  the triplet to result and increase low, decrease high - here checked duplicacy with low and high
+if sum >0 decrease high, if sum<0 increase low */
 
