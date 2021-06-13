d0363d9c44e66b98471cefc1ceec4842c01e2344
pratikshakapse5 <40233432+pratikshakapse5@users.noreply.github.com>
2021-06-12 23:48:51 -0700

    Update sum3.cpp

--- sum3.cpp
+++ sum3.cpp
@@ -5,7 +5,8 @@ I sorted array before which takes O(nlogn) and then putting pivot at 0 and low r
 High pointer runs backwards. for every pivot check the sum of the triplet equals 0, less than 0 or greater than 0
 if sum ==0 add  the triplet to result and increase low, decrease high - here checked duplicacy with low and high
 if sum >0 decrease high, if sum<0 increase low */
-
+//Time complexity: O(n*n)
+//Space complexity: O(1)
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
