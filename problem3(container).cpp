cb4b435324fc3106372911b4911bed3ef38cb385
Akarsh-Chandrashekar <49974714+Akarsh-Chandrashekar@users.noreply.github.com>
2021-06-14 13:52:16 -0700

    Update problem3(container).cpp

--- problem3(container).cpp
+++ problem3(container).cpp
@@ -1,3 +1,10 @@
+// Time Complexity :O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Your code here along with comments explaining your approach:take 2 pointers and move them based on the height record maximum area
+
+
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
