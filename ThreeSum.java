38515068c454bd613624a40f9ba3e26469f5d36d
ngurung91 <nishantgurung.g@gmail.com>
2021-06-13 22:44:32 -0500

    Completed Two Pointers 1

---
+++ ThreeSum.java
@@ -0,0 +1,35 @@
+// Time Complexity : O(n log n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+
+
+public class ThreeSum {
+	public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result  = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i=0; i<n; i++){
+            //outside duplicates
+            if(i!= 0 && nums[i] == nums[i-1]) continue;
+            if (nums[i]>0) break;
+            int left = i+1; int right = n-1;
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    List<Integer> li = Arrays.asList(nums[i], nums[left], nums[right]);
+                    result.add(li);
+                    left++; right--;
+                    //inside duplicates
+                    while(left<right && nums[left] == nums[left-1]) left++;
+                    while(left<right && nums[right] == nums[right+1]) right--;
+                } else if (sum < 0){
+                    left++;
+                }else{
+                    right--;
+                }
+                
+            }       
+        }
+        return result;
+        }
+}
