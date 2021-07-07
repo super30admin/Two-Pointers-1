629042042792957d5a05599b69e07ab99938df8b
jisakp7 <kavya.pathi@jbhunt.com>
2021-07-07 00:15:40 -0500

    SortColors - 1 pass & 2 pass

--- java/SortColors.java
+++ java/SortColors.java
@@ -1,3 +1,48 @@
+1-pass Solution------------------------------------
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+/*
+Taking 2 pointers starting and ending index and checking if 0 then swap its elemnet with first pointer P1 and increment P1
+if element =2 swap it with p2's value and decrement P2 and index
+keep repeating by increasing the index and until its equal to P2
+ */
+
+class Solution {
+    public void sortColors(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        // 1-pass
+        int p1 = 0, p2 = nums.length - 1, index = 0;
+        while (index <= p2) {
+            if (nums[index] == 0) {
+                //if current is 0, swap with p1 pointer, increment p1
+                nums[index] = nums[p1];
+                nums[p1] = 0;
+                p1++;
+            }
+            if (nums[index] == 2) {
+                //if current is 2, swap with p2 pointer, decrement p2
+                nums[index] = nums[p2];
+                nums[p2] = 2;
+                p2--;
+                //may have swapped an extra 0 from the end of array that requires extra processing, so decrement index to account for it
+                index--;
+            }
+            index++;
+        }
+
+    }
+}
+
+
+
+
+
+2pass Solution--------------------------------------------------------
 // Time Complexity : O(N)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : yes
