d6ec4e6676cd642771270c69a4ba98144b60ff57
Tanreet Dhaliwal <tdhali@uwo.ca>
2021-08-02 18:08:15 -0400

    updated two pointer 2

--- ContainerWater.java
+++ ContainerWater.java
@@ -1,14 +1,22 @@
 class Solution {
     public int maxArea(int[] height) {
 
-        //Had some trouble implementing will update
-        int p1 = 0;
-        int p2 = height.length - 1;
-        int max = 0;
+        //O(n) time and O(1) space
 
-        for (int i=0; i<height.length; i++) {
-            Math.max(height[p1] - height[p2]);
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
         }
+
+        return max;
 
     }
 }
--- ThreeSum.java
+++ ThreeSum.java
@@ -1,36 +1,44 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        //Ologn time and o(1) space since hashset is for output
+        //O(n^2) time and O(1) space
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        Set<List<Integer>> res = new HashSet<>();
+        int n = nums.length;
 
-        for (int i=0; i<nums.length; i++) {
+        for (int i=0; i<n; i++) {
 
+            //check for ouside duplicay so when u start another iteration in for loop, if we already did for -1 we dont do it again cuz             //its sorted
+            if (i != 0 && nums[i] == nums[i-1]) continue;
+            //Array is sorted so once we hit only positives we cant make a sum of 0
+            if (nums[i] > 0) break;
             int low = i + 1;
-            int high = nums.length - 1;
+            int high = n - 1;
 
             while (low < high) {
-
-                int sum = nums[low] + nums[high] + nums[i];
+                int sum = nums[i] + nums[low] + nums[high];
 
                 if (sum == 0) {
-                    List<Integer> item = Arrays.asList(nums[low], nums[high], nums[i]);
-                    res.add(item);
+                    //found a triplet
+                    List<Integer> list = Arrays.asList(nums[i], nums[high], nums[low]);
+                    result.add(list);
                     low++;
                     high--;
+                    //keep going till we hit a diff element in array to make sure we dont have duplicate elements
+                    //have to rewrite base condition cuz we move the low and high inside while loop
+                    while (low < high && nums[low] == nums[low - 1]) low++;
+                    while (low < high && nums[high] == nums[high + 1]) high--;
+                    //we can remove the line above because the low pointer will ensure we don't hit any dupliate cases
                 } else if (sum > 0) {
                     high--;
                 } else {
                     low++;
                 }
-
             }
-
 
         }
 
-        return new ArrayList<>(res);
+        return result;
 
     }
 
