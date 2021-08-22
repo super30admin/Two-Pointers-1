2e80b498fea8d92e28970e28b6a74a444d6b068d
Sai Sruthi Mareedu <smareed1@sundevils.asu.edu>
2021-08-22 15:44:34 -0700

    completed

---
+++ water.java
@@ -0,0 +1,32 @@
+// Time Complexity : o(n)
+// Space Complexity :o(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public int maxArea(int[] height) {
+        if(height == null || height.length == 0){
+            return 0;
+        }
+        int max = 0;
+        int low = 0;
+        int high = height.length -1;
+        
+        while(low < high){
+            max = Math.max(max, Math.min(height[low], height[high]) * (high - low));
+            if(height[low] <height[high]){
+                low++;
+            }
+            else{
+                high --;
+            }
+            
+            
+                
+        }
+        return max;
+    }
+}
