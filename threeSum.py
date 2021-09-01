a77742ead195028c5d8379f39699609031c92e92
Smriti0311 <smritigrao@gmail.com>
2021-09-01 17:58:09 -0400

    three sum with 2 pointers

--- threeSum.py
+++ threeSum.py
@@ -1,23 +1,39 @@
-# Time Complexity : O(N^2) for quick sort
+# Time Complexity : O(N^2) for the while loop
 # Space Complexity : O(1)
-# Did this code successfully run on Leetcode : No
-# Any problem you faced while coding this : I was not able to figure out the logic properly
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : I forgot to sort the array
 
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-    res = {}
-    for i in range(len(nums)):
-        if (target - nums[i]) in res:
-            return [nums[i], nums[res[target - nums[i]]]]
-        res[nums[i]] = i
-        
-def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-    res = []
-    for i in range(len(nums)):
-        
-        mid_val = self.twoSum(nums, -1)
-        if nums[i] + sum(mid_val) == 0:
-            mid_val.append(nums[i])
-            res.append(mid_val)
-    return res
+class Solution:
+    def threeSum(self,nums):
+
+        if len(nums) == 0 or len(nums) < 3:
+            return []
+        res = []
+        nums.sort()
+        for i in range(len(nums)):
+
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            lo, hi = i+1, len(nums) - 1
+            while lo < hi:
+
+                mysum = nums[i] + nums[lo] + nums[hi]
+                if mysum == 0:
+                    temp = [nums[i], nums[lo], nums[hi]]
+                    res.append(temp)
+                    lo += 1
+                    hi -= 1
+                    while lo < hi and nums[lo] == nums[lo - 1]:
+                        lo += 1
+                    while lo < hi and nums[hi] == nums[hi+1]:
+                        hi -= 1
+                
+                elif mysum < 0:
+                    lo += 1
+                else:
+                    hi -= 1
+        return res
+
+nums = [-1,0,1,2,-1,-4]
+obj = Solution()
+print(obj.threeSum(nums))
