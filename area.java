01dab95956ed425a8ef3ff3e37c84d5f522c353c
bhesa@DESKTOP-JUA2FT2 <bhesaniayatha@gmail.com>
2021-09-02 22:02:51 -0500

    Completed Sprint Two-Pointers-1

---
+++ area.java
@@ -0,0 +1,17 @@
+public class area {
+    public int maxArea(int[] height) {
+        if(height== null || height.length==0)return 0;
+        int max=0;
+        int low=0;
+        int high=height.length-1;
+        while(low<high){
+            max=Math.max(max,Math.min(height[low],height[high])* (high-low));
+            if(height[low] < height[high]){
+                low++;
+            }else{
+                high--;
+            }
+        }
+        return max;
+    }
+}
