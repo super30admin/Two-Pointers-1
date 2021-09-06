ba06b679a3165d98a6727e8f00bf8dcb4eb64d80
AkashDevendranath <akashdev248@gmail.com>
2021-09-06 08:25:28 -0500

    Two-Pointers-1(Java). Please Review

---
+++ sortColorsArray.java
@@ -0,0 +1,30 @@
+// Time Complexity : O(n) 
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+public class sortColorsArray {
+    public void sortColors(int[] nums) {
+        if(nums == null || nums.length == 0) return;
+        int low = 0; int high = nums.length-1; int mid = 0;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums, mid, low);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 2){
+                swap(nums, mid, high);
+                high--;
+            }
+            else{
+                mid++;
+            }
+        }
+
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
