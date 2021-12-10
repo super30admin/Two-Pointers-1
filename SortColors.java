389657c79f9e81c4d74f7e4b2736008d35d7f663
Taranjit Kaur <taranjit@chegg.com>
2021-12-10 18:06:31 +0530

    Sort colors.

---
+++ SortColors.java
@@ -0,0 +1,33 @@
+public class SortColors {
+
+    // TC - o(n)
+    // space - O(1)
+    // leetcode passed - Yes
+    public void sortColors(int[] nums) {
+        if(nums == null || nums.length ==0) {
+            return;
+        }
+
+        int n = nums.length;
+
+        int low = 0;
+        int mid =0;
+        int high = nums.length-1;
+
+        for(int i =0; i<n;i++) {
+            if(nums[mid] ==0) {
+                swap(nums, low++, mid++);
+            } else if(nums[mid] ==2){
+                swap(nums, high--, mid);
+            } else {
+                mid++;
+            }
+        }
+    }
+
+    void swap(int[] nums, int m, int n) {
+        int temp =nums[m];
+        nums[m] =nums[n];
+        nums[n]=temp;
+    }
+}
