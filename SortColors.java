203fb29f781b8696e242a5a55de62093a4d8ce04
Taranjit Kaur <ktaran14@gmail.com>
2021-12-15 23:25:15 +0530

    Added approach

--- SortColors.java
+++ SortColors.java
@@ -15,12 +15,19 @@ public class SortColors {
         int mid =0;
         int high = nums.length-1;
 
+        // Low is handling 0
+        // mid is handling 1
+        // high is handling 2
         for(int i =0; i<n;i++) {
             if(nums[mid] ==0) {
+                // we swap the two so that 0 comes with low and 1 comes with mid, and we increment them to move further
                 swap(nums, low++, mid++);
             } else if(nums[mid] ==2){
+                // we swap the two so that 2 comes with high and 1 comes with mid, and we decrement high only
+                // because the mid could have 0 with it after the swap
                 swap(nums, high--, mid);
             } else {
+                // mid already have 1
                 mid++;
             }
         }
