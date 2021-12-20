4665559d41aa8a0159d675f6500d67ee4725933d
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2021-12-19 18:36:20 -0800

    Completed Two pointers 1

---
+++ Container.java
@@ -0,0 +1,29 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+// Approach
+
+// Used the pointers approach
+// took two pointers low and high
+// computed the area and too the max at each step
+
+class Solution {
+    public int maxArea(int[] height) {
+        if (height == null || height.length == 0)
+            return 0;
+
+        int low = 0, high = height.length - 1;
+        int max = 0;
+        while (low < high) {
+            max = Math.max(max, Math.min(height[low], height[high]) * (high - low));
+            if (height[low] < height[high]) {
+                low++;
+            } else {
+                high--;
+            }
+        }
+        return max;
+    }
+}
