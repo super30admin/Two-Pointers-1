710424b7ae5500b865302dbf6e9180c35aef850b
Jayavardhan Karampudi <jay@Jais-MacBook-Pro.local>
2022-01-05 21:17:34 -0700

    solved the problems

--- 3sum.py
+++ 3sum.py
@@ -6,16 +6,41 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        if not nums or len(nums) < 3:
-            return []
+        # Optimized - using two pointers
+        nums.sort()
+        n = len(nums)
         result = []
-        hashmap = {}
-        for i in range(len(nums)):
-            val1 = nums[i]
-            for val in range(i, len(nums)):
-                if hashmap.has_key((-1*nums[val])-val1):
-                    result.append([nums[i], nums[val], (-1*nums[val])-val1])
-                    hashmap.pop((-1*nums[val])-val1)
+        for i in range(len(nums)-2):
+            if i!=0 and nums[i] == nums[i-1]: continue
+            low = i+1
+            high = n-1
+            while low < high:
+                sum = nums[i] + nums[low] + nums[high]
+                if sum == 0:
+                    result.append([nums[i], nums[low], nums[high]])
+                    low += 1
+                    high -= 1
+                    while low < high and nums[low] == nums[low-1]:
+                        low += 1
+                    while low < high and nums[low] == nums[high+1]:
+                        high -= 1
+                elif sum < 0:
+                    low += 1
                 else:
-                    hashmap[nums[val]] = val
-        return (result)
+                    high -= 1
+        return result
+                    
+        
+        # if not nums or len(nums) < 3:
+        #     return []
+        # result = []
+        # hashmap = {}
+        # for i in range(len(nums)):
+        #     val1 = nums[i]
+        #     for val in range(i, len(nums)):
+        #         if hashmap.has_key((-1*nums[val])-val1):
+        #             result.append([nums[i], nums[val], (-1*nums[val])-val1])
+        #             hashmap.pop((-1*nums[val])-val1)
+        #         else:
+        #             hashmap[nums[val]] = val
+        # return (result)
