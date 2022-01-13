e83b66e2f5769243f5149f27fb1c95165720253e
gunjalgupta <78285301+gunjalgupta@users.noreply.github.com>
2022-01-13 21:28:20 +0530

    visible

---
+++ WaterContainer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max=0;
+        int low=0;
+        int high = height.length -1;
+        while(low<high){
+            max= Math.max(max, Math.min(height[low], height[high])*(high-low));
+        if(height[low]< height[high])
+            low++;
+        else 
+            high--;
+        }
+        return max;
+    }
+}
