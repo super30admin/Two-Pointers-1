d6e9763fbd22fa969394f77299c018840d75f146
amit singh <amitsingh5317@gmail.com>
2022-02-21 22:35:14 -0600

    amitsingh5317 one pass solution for sort color, its Dutch nation flag solution

--- _75_SortColors.java
+++ _75_SortColors.java
@@ -1,11 +1,15 @@
-// space complexity - o(1)
-    //time - o(2n)
-    //Ran on Leetcode successfully : Yes
-    // Problem faced  : No
-    //Approach : Use two pinter Approach from left and right, move all zeros to left and all non zeros to right
-    // again use two pointer and move all 1 to left and all 2 to right
+
 
 class Solution {
+
+// Two pass solution
+  // space complexity - o(1)
+      //time - o(2n)
+      //Ran on Leetcode successfully : Yes
+      // Problem faced  : No
+      //Approach : Use two pinter Approach from left and right, move all zeros to left and all non zeros to right
+      // again use two pointer and move all 1 to left and all 2 to right
+
     public void sortColors(int[] nums) {
 
         int start = 0 ;
@@ -42,4 +46,33 @@ class Solution {
         }
 
     }
+
+    /**** Brute Force space solution -- for the intution for optimised one pass solution ***/
+    // Now if you see you have three pointers
+   // Running pointer to capture 0 or 2 and ignore 1
+   // if you encoutner 1 then add it to start position in resultant arrya and if you get 2 add it in the end
+   // now we have to do it without using the extra space
+   public void sortColors(int[] nums) {
+
+       int[] res = new int[nums.length];
+
+       int start = 0;
+       int end = nums.length-1;
+       Arrays.fill(res, 1);
+
+       for(int i = 0; i<nums.length; i++){
+               if(nums[i] == 0){
+                   res[start++] = 0;
+               }
+               else if(nums[i] == 2){
+                   res[end--] = 2;
+               }
+       }
+        for(int i = 0; i<nums.length; i++){
+            nums[i] = res[i];
+        }
+
+   }
+
+
 }
