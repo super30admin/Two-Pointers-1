3cddd43464cf283c370b3adf4ab984330caa0fa3
jayachandravvs <42566670+jayachandravvs@users.noreply.github.com>
2022-05-11 08:02:36 -0400

    Update 3Sum.java

--- 3Sum.java
+++ 3Sum.java
@@ -1,1 +1,26 @@
-
+//O(n2) time || O(n) space
+public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
+   
+		List<Integer[]> triplets = new ArrayList<Integer[]>();
+		Arrays.sort(array);
+		for(int i =0; i< array.length-2;i++){
+			int left = i+1;
+			int right= array.length-1;
+			while (left<right){
+					int currentSum = array[i]+array[left]+ array[right];
+				if(currentSum==targetSum){
+					Integer[] newTriplet={array[i],array[left],array[right]};
+					triplets.add(newTriplet);
+					left++;
+					right--;
+				}else if(currentSum<targetSum){
+					left++;
+				}else{
+					right--;
+				}
+			}
+			}
+			return triplets;
+		}
+								
+}
