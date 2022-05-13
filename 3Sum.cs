874996bc0e83eaa3f9331fd863a8cc055401dcf9
Rincy Soman <rincysoman@gmail.com>
2022-05-12 18:04:58 -0700

    3Sum, Sort Colors, ContainerWithMostWater

---
+++ 3Sum.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public class ThreeSumAlg
+    {
+        /// Time Complexity : O(N2)
+        // Space Complexity :O(1)
+        // Did this code successfully run on Leetcode :Yes
+        // Any problem you faced while coding this :  Based on the discussion in class
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            if (nums == null || nums.Length == 0) return result;
+            int n = nums.Length;
+            Array.Sort(nums);
+            for (int i = 0; i < n - 2; i++)
+            {
+                int left = i + 1;
+                int right = n - 1;
+
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+                while (left < right)
+                {
+                    int currentSum = nums[i] + nums[left] + nums[right];
+                    if (currentSum == 0)
+                    {
+                        List<int> sum = new List<int> { nums[i], nums[left], nums[right] };
+                        result.Add(sum);
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1])
+                        {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right + 1])
+                        {
+                            right--;
+                        }
+                    }
+                    else if (currentSum < 0)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
