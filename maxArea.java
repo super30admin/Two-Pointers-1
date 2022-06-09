938fe72c41ef2b0b4e6cc7143d5a1d56ae14b700
umang_patel <umang.patel@alshaya.com>
2022-06-09 22:37:17 +0530

    threesum completed

--- maxArea.java
+++ maxArea.java
@@ -1,3 +1,8 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// Did this code successfully run on Leetcode: yes
+// Any problem you faced while coding this: took ref from lecture video
+
 public class maxArea {
 
     public static int getMaxArea(int[] height) {
