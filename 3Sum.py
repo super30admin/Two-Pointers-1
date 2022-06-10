25f2b83f5ae6ea00ecbf11dc3774d06ab6736fc5
Kedar Kulkarni <kedarvijayk@kedarvijayk-a01.vmware.com>
2022-06-09 23:45:54 -0400

    added third approach

--- 3Sum.py
+++ 3Sum.py
@@ -68,3 +68,47 @@ class Solution:
                 else:
                     result.add(tuple(sorted((nums[i], nums[j], (target-nums[j])))))
         return list(result)
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        '''Approach 3: Two Pointers - best - over HashMap and Binary Search
+            Sort Array : O(nlogn) + O(n^2)
+            Space: O(1)
+            Then use 2 pointers solution
+            To remove internal duplicacy in 2Sum , as soon as you find 1st pair of 2Sum, 
+            Keep moving both pointers until you find unique elements  
+            Outside duplicacy can be handled by moving outer pointer forward one step 
+            at a time until a new element is found. Remember to break out of loop
+            when nums[i] is >0 because after that point there are no more negative elements
+            as we sorted the array so we can't find any combination of 3Sum =0.
+        '''
+        nums = sorted(nums)
+        result = list()
+        for i in range(len(nums)-2):
+             # while nums[i-1] and nums[i] is same, keep moving i forward
+            # to avoid duplicate results - it will cause duplicate in the 1st index of tuple otherwise
+            # like this: 
+            # [[-1,-1,2],[-1,0,1],[-1,0,1]]
+            if i > 0 and nums[i-1] == nums[i]: continue
+            # if num[i] >0 then low and high pointers can't give me any <0 nums
+            # hence sum will never be 0, so break the loop
+            if  nums[i] > 0 : break
+           
+
+            low = i+1
+            high = len(nums) -1
+            while low < high:
+                ssum = nums [i] + nums[low] + nums[high]
+                if ssum == 0:
+                    result.append([nums[i], nums[low], nums[high]])                    
+                    low +=1
+                    high -=1
+                    # remove interal duplicacy
+                    while low < high and nums[low-1] == nums[low] and low<high: low +=1
+                    while low < high and nums[high+1] == nums[high] and low<high: high -=1
+                    
+                elif ssum < 0:
+                    low +=1
+                else:
+                    high -=1
+        return result
