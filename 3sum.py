96c1d3b0c89a85fee6c1b3cd5428f39a44c70d20
Jayavani Akkiraju <jayavaniakkiraju@Jayavanis-MacBook-Pro.local>
2022-06-17 14:46:15 +0530

    Two Pointers-1 completed

--- 3sum.py
+++ 3sum.py
@@ -1,1 +1,35 @@
-
+'''
+-- Passed Test Cases on LeetCode
+'''
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    #O(n^2) -- Time Complexity; O(logn) -- Best case Space complexity and O(n) Worst case Space complexity
+        res = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i - 1] != nums[i]:
+                self.twoSum(nums, i, res)
+        return res
+                
+    def twoSum(self, nums, i, res):
+        low = i + 1
+        high = len(nums) - 1
+        while(low < high):
+            sum = nums[i] + nums[low] + nums[high]
+            if sum < 0:
+                low += 1
+            elif sum > 0:
+                high -= 1
+            else:
+                res.append([nums[i], nums[low], nums[high]])
+                low += 1
+                high -= 1
+                while low < high and nums[low] == nums[low - 1]:
+                    low += 1
+                    
+        
+        
+        
+        
--- containerWithMostWater.py
+++ containerWithMostWater.py
@@ -1,1 +1,30 @@
+'''
+-- Passed test cases on LeetCode
+'''
+class Solution:
 
+    def maxArea(self, height: List[int]) -> int:
+# Two Pointer Approach
+#Time Complexity - O(n) as we do n number of comparisions between the low and high pointers ; Space Complexity - O(1)
+
+        maxim = 0
+        low = 0
+        high = len(height) - 1
+        for i in range(low, high):
+            maxim = max(maxim, min(height[low],height[high]) * (high - low))
+            if height[low] <= height[high]:
+                low += 1
+            else:
+                high -= 1
+        return maxim    
+        
+# Brute Force Approach
+#Time Complexity - O(n^2); Space Complexity - O(1)
+        ''''
+        maxim = 0
+        for i in range(len(height)):
+            for j in range(i+1, len(height)):
+                maxim = max(maxim, min(height[i], height[j]) * (j-i))
+        return maxim
+        '''
+        
