219a6805fdec48d2605dd9e1138f36420c87fc3c
prasannaOO7 <70802340+prasannaOO7@users.noreply.github.com>
2022-07-07 21:51:51 -0400

    completed 3 sum and most water

---
+++ 3sum.java
@@ -0,0 +1,40 @@
+// Time Complexity : o(n^2)
+// Space Complexity : o(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            int left=i+1;
+            int right=nums.length-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum>0){
+                    right--;
+                }else if(sum<0){
+                    left ++;
+                }else{
+                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    left++;
+                    right--;
+                    while(left<right && nums[left]==nums[left-1]){
+                        left++;
+                    }
+                    while(left<right && nums[right]==nums[right+1]){
+                        right--;
+                    }
+                }
+            }
+        }
+    return result;
+    }
+}
