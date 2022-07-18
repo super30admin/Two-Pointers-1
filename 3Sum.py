78e5ec6f7911a921c5c067c32de7e27c04af530b
Mani9333 <90652612+Mani9333@users.noreply.github.com>
2022-07-18 10:17:33 -0700

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -33,7 +33,46 @@ class Solution:
                     l += 1
                 else:
                     r -= 1
+                    
+                   
         return res
                     
-                
+ 
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        result = list()
+        
+        if nums == [] or len(nums) == 0:
+            return result
+        
+        nums.sort()
+        
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            l,r = i+1,len(nums)-1
+            
+            while l < r:
+                curr = nums[i]+nums[l]+nums[r]
+                if curr == 0:
+                    result.append([nums[i],nums[l],nums[r]])
+                    l += 1
+                    r -= 1
+                    while l<r and nums[l] == nums[l-1]:
+                        l += 1
+                    while l<r and nums[r] == nums[r+1]:
+                        r -= 1
+                elif curr > 0:
+                    r -= 1
+                    while l<r and nums[r] == nums[r+1]:
+                        r -= 1
+                else:
+                    l += 1
+                    while l<r and nums[l] == nums[l-1]:
+                        l += 1
+        return result
         
