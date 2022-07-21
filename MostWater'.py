93d4672c1b4243e7db740f10f42b69d8b123e975
Raj Shah <rajshah1998.d@gmail.com>
2022-07-21 15:00:21 -0400

    Completed Two-Pointers-1

---
+++ MostWater'.py
@@ -0,0 +1,18 @@
+'''
+Time Complexity: O(n)
+Space COmplexity: O(1)
+Run on Leetcode: YES
+'''
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        low = 0
+        high = len(height) - 1
+        areaMax = 0
+        while low < high:
+            if height[low] < height[high]:
+                areaMax = max(areaMax, height[low] * (high - low))
+                low += 1
+            else:
+                areaMax = max(areaMax, height[high] * (high - low))
+                high -= 1
+        return areaMax 
