9d8e1b341086a81d326f84bf73b366196599cd11
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-08-04 22:33:24 -0400

    Update SortColor.java

--- SortColor.java
+++ SortColor.java
@@ -1,3 +1,7 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 class Solution {
     public void sortColors(int[] nums) {
         int start =0;
