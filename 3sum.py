da0f862a476c72acd5d285f23b97a6bc8e448c68
KeerthanaThonupunuri <keerthanathonupunuri.com>
2022-09-19 15:09:22 -0700

    completed Two-pointers

---
+++ 3sum.py
@@ -0,0 +1,24 @@
+#executed on leetcode
+#time complexity - O(n^2)
+#space complexity - O(n)
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n=len(nums)
+        res=[]
+        nums.sort()
+        for i, a in enumerate(nums):
+            if i>0 and a==nums[i-1]:
+                continue
+            l,r=i+1,n-1
+            while l<r:
+                threeSum=a+nums[l]+nums[r]
+                if threeSum>0:
+                    r-=1
+                elif threeSum<0:
+                    l+=1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l+=1
+                    while(nums[l]==nums[l-1]) and l<r:
+                        l+=1
+        return res
