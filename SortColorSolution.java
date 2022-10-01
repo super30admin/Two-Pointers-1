e07241713ba9b5da9f900a48ced93108faf0d740
Sumanth Shankar <sumanth@rezy360.com>
2022-10-01 14:21:21 -0700

    Solution for sort colors

--- SortColorSolution.java
+++ SortColorSolution.java
@@ -1,8 +1,38 @@
-// Time Complexity :
-// Space Complexity :
+// Time Complexity : O(n)
+// Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
+//Using 2 pointer approach. On encountering 2 don't update mid just reduce high.
+// On encountering 0 update low and mid.
+// On encountering 1 update just mid.
 public class SortColorSolution {
+    public void sortColors(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return;
+        }
+        
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        
+        while(mid <= high) {
+            if(nums[mid] == 2) {
+                swap(nums, mid, high);
+                high--;
+            } else if(nums[mid] == 0) {
+                swap(nums, mid, low);
+                low++;
+                mid++;
+            } else {
+                mid++;
+            }
+        }
+    }
     
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
