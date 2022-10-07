f46abdc6a708298c6496c0b79c2d7b7e9f0c40c2
Samrita Sen Bakshi <samrita.bakshe@gmail.com>
2022-10-06 17:42:29 -0700

    Problem 1,2 &3 done

---
+++ problem3.js
@@ -0,0 +1,12 @@
+//Time: O(n), Space: O(1)
+var maxArea = function(height) {
+    let maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+};
