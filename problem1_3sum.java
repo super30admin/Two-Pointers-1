8a928031953c1e9ef9555e7cec0a118cc2be0663
Mitalee Minde <mitaleeminde@gmail.com>
2022-10-27 10:50:13 -0400

    changed time and space complexity for 3sum

--- problem1_3sum.java
+++ problem1_3sum.java
@@ -1,5 +1,5 @@
-// Time Complexity : O(n^2)
-// Space Complexity : O(1)
+// Time Complexity : O(n)
+// Space Complexity : O(n)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
