3716c503d67d5d0e6754c223ff8ab24da20ba798
Riddhi Jaju <riddhijaju2610@gmail.com>
2022-11-17 19:07:32 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -40,3 +40,38 @@ Problem 75- sort colors
         }    
     }
 }
+
+
+
+
+Problem 11- container with more water
+// Time Complexity :O(n)
+// Space Complexity :constant
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
+
+class Solution {
+    public int maxArea(int[] height) {
+        if(height==null || height.length==0) return 0;
+        int m=0;
+        int p1,p2;
+        p1=0;
+        p2=height.length-1;
+        
+        while(p1<p2)
+        {
+            if(height[p1]>height[p2])
+            {
+                m= Math.max(m,height[p2]*(p2-p1));
+                p2--;
+            }
+            else
+            {
+                m= Math.max(m,height[p1]*(p2-p1));
+                p1++;
+            }
+        }
+        return m;
+        
+    }
+}
