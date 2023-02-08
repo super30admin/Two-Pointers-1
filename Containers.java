e8d74af00c1827b5477fe665a740adfd0eee484c
Manthan Wadiwala <manthancar@gmail.com>
2023-02-08 09:26:34 -0500

    Completed Two-Pointers-1

---
+++ Containers.java
@@ -0,0 +1,21 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            int currArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currArea);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+}
