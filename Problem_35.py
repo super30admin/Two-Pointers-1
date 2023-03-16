2f113e6608eaeaed1c12f69ff37ad14f1c24c8da
Shivangi Shakya <shivangi@Shivangis-MBP.lan>
2023-03-15 18:16:49 -0700

    Completed all Problems

--- Problem_35.py
+++ Problem_35.py
@@ -1,18 +1,19 @@
-# TC = O(n)
-# SC = O(3) = O(1)
-
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        dict1 = {0: 0, 1: 0, 2: 0}
-        
-        for i in nums:
-            dict1[i] += 1
-        
-        nums.clear()
-        for i in dict1:
-            while dict1[i] != 0:
-                nums.append(i)
-                dict1[i] -= 1
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 2:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+            elif nums[mid] == 0:
+                nums[mid], nums[low] = nums[low], nums[mid]
+                low += 1  
+                mid += 1
+            else:
+                mid += 1            
