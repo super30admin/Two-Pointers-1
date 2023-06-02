7fa528cbd93c4c33658b0bf82942225c6e69d650
PRAYAG-PATEL-1998 <90693541+PRAYAG-PATEL-1998@users.noreply.github.com>
2023-06-01 17:34:38 -0700

    Update Sample.java
    Two-pointers-1 completed

--- Sample.java
+++ Sample.java
@@ -1,7 +1,88 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+class Solution {
+    public void sortColors(int[] nums) {
+        if(nums == null || nums .length == 0)return;
+        int low = 0;
+        int mid = 0;
+        int high = nums.length -1;
+        while(mid <= high){
+            if(nums[mid] == 2){
+                swap(mid,high,nums);
+                high--;
+            }else if(nums[mid] == 0){
+                swap(mid,low,nums);
+                low++;
+                mid++;
+            }
+            else{
+                mid++;
+            }
+        }
+    }
+    public void swap(int i , int j, int[] nums){
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+        System.out.println(""+nums[i]+""+nums[j]);
+    }
+}
 
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int l = nums.length;
+        //3sum
+        for(int i=0; i<l-2; i++){
+            //removing external duplicates
+            if(i!=0 && nums[i] == nums[i-1]) continue;
+            //2sum
+            int com = 0-nums[i];
+            int j = i+1;
+            int k = l-1;
+            while(j<k){
+                int sum = nums[j]+nums[k];
+                if(sum > com){
+                    k--;
+                    
+                }else if(sum < com){
+                    j++;
+                    
+                }else{
+                    result.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    j++;
+                    k--;
+                    while(j<k && nums[k] == nums[k+1]){
+                        k--;
+                    }
+                    while(j<k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+                }
+            }
+            
+        }
+        return result;
+    }
+}
 
-// Your code here along with comments explaining your approach
+class Solution {
+    public int maxArea(int[] height) {
+        if(height == null || height.length == 0) return 0;
+        int max = 0;
+        int low = 0;
+        int high = height.length - 1;
+        while(low < high){
+            max = Math.max(max,Math.min(height[low],height[high]) * (high - low));
+            if(height[low] < height[high]){
+                low++;
+            }else if (height[low] == height[high]){
+                low++;
+                high--;
+            }
+            else{                
+                high--;
+            }
+        }
+        return max;
+    }
+}
