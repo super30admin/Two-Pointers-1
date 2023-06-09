63355a1ae968377199db261875406adcbc4f8511
Debdyuti Das <debdyuti@Debdyutis-MacBook-Air.local>
2023-06-09 10:07:38 -0700

    Completed two pointers 1

---
+++ MaximumWater.java
@@ -0,0 +1,14 @@
+// Time complexity: O(n)
+// Space complexity: O(1);
+class Solution {
+    public int maxArea(int[] height) {
+        int l=0,r=height.length-1;
+        int max=0;
+        while(l<r){
+        max=Math.max(max,Math.min(height[l],height[r])*(r-l));
+        if(height[l] <= height[r]) l++;
+        else r--;
+        }
+        return max;
+    }
+}
