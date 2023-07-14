014c34c8229eb75a346d663a3e5ff552c603762d
Hemanth Reddy <hemanthreddy5247@gmail.com>
2023-07-13 21:56:03 -0500

    Completed Two-Pointers-1

---
+++ Problem1.py
@@ -0,0 +1,16 @@
+class Solution:
+    # Time Complexity:O(n)
+    # Space Complexity:O(1)
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+
+        while left < right:
+            current_area = min(height[left], height[right]) * (right - left)
+            max_area = max(max_area, current_area)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_area
