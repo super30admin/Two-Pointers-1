04268ff71cc37cfcf921dce7c6f83f8cfb6373e5
Satwik Mallajosyula <satwikm@Satwiks-MBP.lan>
2023-07-15 17:29:10 -0700

    completed 2-pointers -1

---
+++ 3Sum.cpp
@@ -0,0 +1,72 @@
+// Time Complexity :O()
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        vector<vector<int>> result;
+
+        
+        
+
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<nums.size()-1;i++){
+            int left = i+1;
+            int right = nums.size()-1;
+
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+
+            }
+
+            while(left<right)
+            {
+
+                int threeSum = nums[i]+nums[left]+nums[right];
+                
+                
+
+                if(threeSum>0){
+                    right--;
+                }
+
+                else if(threeSum<0){
+                    left++;
+                }
+
+                else{
+                    result.push_back({nums[i],nums[left],nums[right]});
+                    left++;
+                    while(nums[left]==nums[left-1] && left < right){
+
+                        left++;
+
+                    }
+
+                }
+            
+            
+            }
+
+
+            
+
+        }
+
+
+        return result;
+        
+    }
+};
---
+++ ContainerMostWater.cpp
@@ -0,0 +1,50 @@
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+
+
+        int max=0;
+        int curr;
+        
+
+       int first=0;
+       int last = height.size()-1;
+
+       while(first<last){
+           
+           if(height[first]<height[last]){
+               curr=height[first]*(last-first);
+               first++;
+               
+
+           }
+           else{
+               curr=height[last]*(last-first);
+               last--;
+
+           }
+
+           if(curr>max){
+               max=curr;
+           }
+       }
+
+       return max;
+
+        
+
+        
+    }
+};
