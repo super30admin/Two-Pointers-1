186056c7d4e4f7bf81918fc91f215ceb2582b92f
lavanyareddy21 <lavanyareddy2095@gmail.com>
2023-07-19 10:24:16 -0700

    Completed

--- problem2.py
+++ problem2.py
@@ -1,7 +1,7 @@
 # 2sum
 # Approach1: brute force 
 # Time Complexity: O(n^2)
-# Space Complexity: O(1)
+# Space Complexity: O(n^2)
 nums = [4,9,1,0,2,3,5,6]
 target = 7
 result = []
@@ -10,12 +10,31 @@ for i in range(len(nums)):
         if nums[i] + nums[j] == target:
             result.append([nums[i],nums[j]])  #if asked for indices result.append([i,j])
 print(result)
+# 3sum
+# Approach1: brute force 
+# Time Complexity: O(n^3)
+# Space Complexity: O(n^3)
+nums = [-1, 0, 1, 2, -1, -4]
+target = 0
+result = []
+n = len(nums)
+for i in range(n - 2):
+    for j in range(i + 1, n - 1):
+        for k in range(j + 1, n):
+            if nums[i] + nums[j] + nums[k] == target:
+                triplet = [nums[i], nums[j], nums[k]]
+                triplet.sort()  
+                if triplet not in result:  # checking for duplicates
+                    result.append(triplet)
+print(result)
 
 
 
+
+# 2Sum
 # Approach2: sort and 2 pointers 
-# Time Complexity: O(nlogn)
-# Space Complexity: O(1)
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 nums = [4,9,1,0,2,3,5,6]
 target = 7
 result = []
@@ -31,7 +50,40 @@ while left < right:
 
 print(result)
 
+# 3Sum
+# Approach2: sort and 2 pointers 
+# Time Complexity: O(n^2)
+# Space Complexity: O(n^2)
+nums = [-1, 0, 1, 2, -1, -4]
+target = 0
+result = []
+n = len(nums)
+nums.sort()  #nlogn
+for i in range(n - 2): #n
+    # Skip duplicate values for i
+    if i > 0 and nums[i] == nums[i - 1]:
+        continue
+    left = i + 1
+    right = n - 1
+    while left < right: #n
+        current_sum = nums[i] + nums[left] + nums[right]
+        if current_sum == target:
+            result.append([nums[i], nums[left], nums[right]])
+            left += 1
+            right -= 1
+            # check duplicates
+            while left < right and nums[left] == nums[left - 1]:
+                left += 1
+            while left < right and nums[right] == nums[right + 1]:
+                right -= 1
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+print(result)
 
+
+# 2Sum
 # Approach3: Using hashmap
 # Time Complexity: O(n)
 # Space Complexity: O(n)
@@ -49,6 +101,8 @@ print(result)
 
 
 
+
+# 2Sum
 # Approach4: Using hashset
 # Time Complexity: O(n)
 # Space Complexity: O(n)
@@ -64,21 +118,9 @@ for i in range(len(nums)):
 print(result)
 
 
-# 3 Sum
-# Time Complexity: O(n^3)
-# Space Complexity: O(n^3)
-nums = [-1, 0, 1, 2, -1, -4]
-target = 0
-result = []
-n = len(nums)
-for i in range(n - 2):
-    for j in range(i + 1, n - 1):
-        for k in range(j + 1, n):
-            if nums[i] + nums[j] + nums[k] == 0:
-                triplet = [nums[i], nums[j], nums[k]]
-                triplet.sort()  
-                if triplet not in result:
-                    result.append(triplet)
-print(result)
+
+
+
+
 
 
