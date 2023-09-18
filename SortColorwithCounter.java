24fa2b0a806cb0ed4fb20018c639bb32beb0d2bf
UmangThakur15 <umangthakuremail@gmail.com>
2023-09-18 12:11:21 -0400

    Sort colors with counter

---
+++ SortColorwithCounter.java
@@ -0,0 +1,47 @@
+class SortColorwithCounter {
+
+    //TC will be O(n), as traversing only once.
+    //SC will be O(1).
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        for(int i=0; i<n;i++){
+            if(nums[i]==0){
+                count1++;
+            }
+            else if(nums[i]==1){
+                count2++;
+            }
+            else{
+                count3++;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(count1>0){
+                nums[i]=0;
+                count1--;
+            }
+            else if(count2>0){
+                nums[i]=1;
+                count2--;
+            }
+            else{
+                nums[i]=2;
+                count3--;
+            }
+            System.out.println(nums[i]);
+        }
+
+
+
+    }
+
+    public static void main(String[] args){
+        SortColorwithCounter obj = new SortColorwithCounter();
+        int[] nums = {2,0,2,1,1,0};
+        obj.sortColors(nums);
+    }
+}
