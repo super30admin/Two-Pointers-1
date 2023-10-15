d2cbd0bfd78b3cac5e3d7a5a94c31f41b444aa69
Yatish Koul <36380820+yatishkoul@users.noreply.github.com>
2023-10-15 01:54:36 -0700

    Create Sort_Colours.cpp

---
+++ Sort_Colours.cpp
@@ -0,0 +1,22 @@
+// Dutch Flag Algorithm
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+      int n=nums.size();
+      int low=0, high = n-1, mid=0;
+      while(mid<=high){
+          if(nums[mid]==0){
+              swap(nums[low], nums[mid]);
+              low++;
+              mid++;
+          }
+          else if(nums[mid]==2){
+              swap(nums[high], nums[mid]);
+              high--;
+          }
+          else{
+              mid++;
+          }
+      }  
+    }
+};
