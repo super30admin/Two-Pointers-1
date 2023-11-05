b14acd6caee4460c9f274d72c6c3f4302c2e2f89
Vivek Reddy Sripathi <viveksripathi5312@gmail.com>
2023-11-05 01:04:10 -0600

    update problem 3

--- Problem3.java
+++ Problem3.java
@@ -1,5 +1,12 @@
-//TC=O(n)
-//SC=O(1)
+// Time Complexity :  O(n)
+// Space Complexity :  O(1)
+// Did this code successfully run on Leetcode :  Yes
+// Any problem you faced while coding this :  No
+
+
+// increasing the width of the container can only lead to a larger area if the height of the new boundary is greater.
+// By following this condition and moving the pointers accordingly, the algorithm explores all possible containers and
+// finds the one with the maximum area.
 
 class Solution {
     public int maxArea(int[] height) {
