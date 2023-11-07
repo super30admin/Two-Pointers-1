52f06cf0d4ebaf3b3c33a3f87190bf792d137edc
Aditya Kondepudi <36687530+adityakondepudi@users.noreply.github.com>
2023-11-07 14:07:32 -0500

    Create Problem3.py

---
+++ Problem3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r, area = 0, len(height) - 1, 0
+        while l < r:
+            area = max(area, (r - l) * min(height[l], height[r]))
+            if height[l] < height[r]:
+                l=l+1
+            else:
+                r=r-1
+        return area
