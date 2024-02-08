2a2c50d6bac9710aff7d2470d08e30c34f1cb7fe
kpranavreddy01 <114799260+kpranavreddy01@users.noreply.github.com>
2024-02-08 22:04:21 +0530

    pointers problems solved

---
+++ container.java
@@ -0,0 +1,22 @@
+// Time Complexity :O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
+
+class Solution {
+    public int maxArea(int[] height) {
+        int max=0;
+        int n = height.length;
+        int low =0; int high=n-1;
+        while(low<=high){
+            int currAr=Math.min(height[low],height[high])*(high-low);
+            max = Math.max(max , currAr);
+            if(height[low]<=height[high]){
+                low++;
+            }else {
+                high--;
+            }
+        }
+        return max;
+    }
+}
