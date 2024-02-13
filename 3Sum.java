3c005c394d2b219401a3da442e3beafc03bbdd10
Anupama Ponukumati <anupamaponukumati@Anupamas-MacBook-Air.local>
2024-02-12 22:39:23 -0800

    3sum and Container with most water

---
+++ 3Sum.java
@@ -0,0 +1,34 @@
+
+## Problem2 (https://leetcode.com/problems/3sum/)
+
+// Time Complexity : O(n2)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length && nums[i] <= 0; ++i)
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSum(nums, i, res);
+            }
+        return res;
+    }
+    void twoSum(int[] nums, int i, List<List<Integer>> res) {
+        var seen = new HashSet<Integer>();
+        for (int j = i + 1; j < nums.length; ++j) {
+            int complement = -nums[i] - nums[j];
+            if (seen.contains(complement)) {
+                res.add(Arrays.asList(nums[i], nums[j], complement));
+                while (j + 1 < nums.length && nums[j] == nums[j + 1])
+                    ++j;
+            }
+            seen.add(nums[j]);
+        }
+    }
+}
