0766446c959c83e2f3ae50ef8c5e0c5806c7346e
samirshah120 <samirshah120@gmail.com>
2024-03-14 16:10:35 -0400

    two pointers 1 samir

---
+++ SortColors.java
@@ -0,0 +1,30 @@
+//tc = o(n)
+//sc = o(1)
+class Solution {
+    private void swap(int[] nums, int v1, int v2){
+        int temp = nums[v1];
+        nums[v1] = nums[v2];
+        nums[v2] = temp;
+
+    }
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
+        while(mid <= high){
+            if(nums[mid] == 1){
+                mid++;
+            }
+            else if(nums[mid] == 0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums,mid,high);
+                high--;
+            }
+
+        }
+    }
+}
