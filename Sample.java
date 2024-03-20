e58852132a29d19cac2190be1a5cf3b8660a470f
Dhanashree Lale <dhanashreelale@Dhanashrees-MacBook-Pro.local>
2024-03-20 10:47:53 -0500

    completed two pointers 1

--- Sample.java
+++ Sample.java
@@ -1,32 +1,7 @@
-// Time Complexity : O(n)
-// Space Complexity : O(1) 
-// Did this code successfully run on Leetcode : Yes
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
 
-class Solution {
-    public void sortColors(int[] nums) {
-        if( nums == null || nums.length == 0)
-        return;
 
-        int n = nums.length;
-        int low = 0, mid = 0, high = n-1;
-
-        while(mid <= high){
-            if(nums[mid] == 0){
-                swap(nums, mid, low);
-                low++;
-                mid++;
-            }else if(nums[mid] == 2){
-                swap(nums, mid, high);
-                high--;
-            }else{
-                mid++;
-            }
-        }
-    }
-
-    private void swap(int nums[], int i, int j){
-        int temp = nums[i] ;
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-}
+// Your code here along with comments explaining your approach
