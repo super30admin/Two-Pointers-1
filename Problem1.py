d64f9c1bc43ab18e4beda1954eb5259c14a4158d
jeethirakani20 <148606277+jeethirakani20@users.noreply.github.com>
2024-03-21 10:11:32 -0700

    Problem1 completed

---
+++ Problem1.py
@@ -0,0 +1,24 @@
+#T.C. O(n)
+#S.C. O(1)
+
+#Approach : Dutch National Flag Algorithm
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+        
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            elif nums[mid] == 2:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+        
